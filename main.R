9bed3e0a65444c38f1ce8b003f2888545da25dfb
Iru Díaz García <108950915+irudiazgarcia@users.noreply.github.com>
2023-10-16 19:40:33 +0200

    Add files via upload

--- main.R
+++ main.R
@@ -2,50 +2,73 @@ library(vitae)
 library(dplyr)
 library(magrittr)
 source("Utils/data_download_funs.R")
-
-my_data_location = "https://docs.google.com/spreadsheets/d/1KRBDSb8F8fOS9nQpcMOPTNqzA6FCjNxvLfWZsv0eunk/edit?usp=sharing"
-
+library(shiny)
 
 
-CV <- create_CV_object(
-  data_location = my_data_location,  
-  pdf_mode = FALSE
+ui <- fluidPage(
+  titlePanel("CV3: Título v1.0"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      textInput("my_data_location", "Enlace al Google Sheet:", value = ""),
+      actionButton("generate", "Generar CV")
+    ),
+    
+    mainPanel(
+      verbatimTextOutput("result")
+    )
+  )
 )
 
 
-parames = list(
-  my_name =  as.character(CV$contact_info[CV$contact_info$loc == "name","contact"]),
-  my_surname =  as.character(CV$contact_info[CV$contact_info$loc == "surname","contact"]),
-  my_address =  as.character(CV$contact_info[CV$contact_info$loc == "address","contact"]),
-  my_position =  as.character(CV$contact_info[CV$contact_info$loc == "position","contact"]),
-  my_mobile =  as.character(CV$contact_info[CV$contact_info$loc == "mobile","contact"]),
-  my_phone =  as.character(CV$contact_info[CV$contact_info$loc == "phone","contact"]),
-  my_email = as.character(CV$contact_info[CV$contact_info$loc == "email","contact"]),
-  my_github = as.character(CV$contact_info[CV$contact_info$loc == "github","contact"]),
-  my_linkedin = as.character(CV$contact_info[CV$contact_info$loc == "linkedin","contact"]),
-  my_data_location = my_data_location,
-  my_profilepic = as.character(CV$contact_info[CV$contact_info$loc == "profilepic","contact"]),
-  my_headcolor = as.character(CV$contact_info[CV$contact_info$loc == "headcolor","contact"])
-)
+server <- function(input, output) {
+  
+  result <- reactive({ 
+    if (input$generate > 0) {
+      
+      CV <- create_CV_object(
+        data_location = input$my_data_location,
+        pdf_mode = FALSE
+      )
+      
+      parames = list(
+        my_name =  as.character(CV$contact_info[CV$contact_info$loc == "name","contact"]),
+        my_surname =  as.character(CV$contact_info[CV$contact_info$loc == "surname","contact"]),
+        my_address =  as.character(CV$contact_info[CV$contact_info$loc == "address","contact"]),
+        my_position =  as.character(CV$contact_info[CV$contact_info$loc == "position","contact"]),
+        my_mobile =  as.character(CV$contact_info[CV$contact_info$loc == "mobile","contact"]),
+        my_phone =  as.character(CV$contact_info[CV$contact_info$loc == "phone","contact"]),
+        my_email = as.character(CV$contact_info[CV$contact_info$loc == "email","contact"]),
+        my_profilepic = as.character(CV$contact_info[CV$contact_info$loc == "profilepic","contact"]),
+        my_headcolor = as.character(CV$contact_info[CV$contact_info$loc == "headcolor","contact"]),
+        my_fontsize = as.character(CV$contact_info[CV$contact_info$loc == "fontsize","contact"]),
+        
+        my_linkedin = as.character(CV$contact_info[CV$contact_info$loc == "linkedin","contact"]),      #OPCIONAL
+        my_github = as.character(CV$contact_info[CV$contact_info$loc == "github","contact"]),          #OPCIONAL
+    
+        my_data_location = input$my_data_location
+      )
+      
+      
+      rm(CV)
+      
+      rmarkdown::render('CV3.Rmd', params = parames, output_file = paste("CV",parames$my_name, chartr(" ", "_",parames$my_surname), sep="_"))
+      
+      
+      return(0)
+    }
+  })
+  
+  output$result <- renderText({
+    result()  # Execute the reactive block to trigger the action
+    # You can return any text output here if needed
+    return("Data downloaded and processed.")
+  })
+}
+
+shinyApp(ui, server)
 
 
-rm(CV)
-
-rmarkdown::render('CV3.Rmd', params = parames, output_file = paste("CV",parames$my_name, chartr(" ", "_",parames$my_surname), sep="_"))
 
 
-#Para añadir nuevos espacios: añadir este texto modificado al gusto en el archivo CV3.Rmd
-#
-## Voluntariado
 
-#```{r}
-#CV$entries_data %>% 
-#  filter(section == 'voluntary') %>% 
-#  detailed_entries(
-#    what = institution,
-#    when = glue::glue("{`start`} - {`end`}"),
-#    with = title,
-#    where = loc,
-#    why = description_bullets
-#  )
-#```
